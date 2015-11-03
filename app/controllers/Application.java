76fc780c91b960da2300fd9585aacbc49374c348
joselaraangel <jose.lara.angel@gmail.com>
2015-11-03 00:52:58 -0600

    3-> primera pantalla

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.Play.*;
 import play.mvc.*;
 
 import java.util.*;
@@ -10,7 +11,17 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+    
+    @Before
+    static void addDefaults() {
+    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
 
 }
