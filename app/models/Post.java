5728334edc9e8a5a51a2a01c612870da19006dce
joselaraangel <jose.lara.angel@gmail.com>
2015-11-03 23:10:23 -0600

    6-> agregando soporte para tags

--- app/models/Post.java
+++ app/models/Post.java
@@ -20,13 +20,17 @@ public class Post extends Model {
     @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
     public List<Comment> comments;
     
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    public Set<Tag> tags;
+    
     public Post(User author, String title, String content) {
-        this.comments = new ArrayList<Comment>();
-        this.author = author;
-        this.title = title;
-        this.content = content;
-        this.postedAt = new Date();
-    }
+    this.comments = new ArrayList<Comment>();
+    this.tags = new TreeSet<Tag>();
+    this.author = author;
+    this.title = title;
+    this.content = content;
+    this.postedAt = new Date();
+}
     
     public Post addComment(String author, String content) {
         Comment newComment = new Comment(this, author, content).save();
@@ -41,6 +45,17 @@ public class Post extends Model {
  
     public Post next() {
     return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+    }
+    
+    public Post tagItWith(String name) {
+    tags.add(Tag.findOrCreateByName(name));
+    return this;
+    }
+    
+    public static List<Post> findTaggedWith(String tag) {
+    return Post.find(
+        "select distinct p from Post p join p.tags as t where t.name = ?", tag
+    ).fetch();
     }
  
 }
